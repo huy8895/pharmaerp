dd49899624bb5c6b8418ef881af7f8460a879332
Tong Van Duc <tongduc315@gmail.com>
2023-02-04 21:25:56 +0700

    Initial commit

---
+++ src/main/java/DKSPACE/PhamarERP/PhamarErpApplication.java
@@ -0,0 +1,13 @@
+package DKSPACE.PhamarERP;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PhamarErpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PhamarErpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/DKSPACE/PhamarERP/PhamarErpApplicationTests.java
@@ -0,0 +1,13 @@
+package DKSPACE.PhamarERP;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PhamarErpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
