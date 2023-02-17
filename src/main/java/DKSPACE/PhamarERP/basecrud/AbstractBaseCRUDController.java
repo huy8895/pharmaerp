15a19946065209dfa7abef734643bf0b0e29e24e
huy8895 <huytv.ous@mcredit.com.vn>
2023-02-17 10:53:38 +0700

    [BaseCRUD]

---
+++ src/main/java/DKSPACE/PhamarERP/basecrud/AbstractBaseCRUDController.java
@@ -0,0 +1,34 @@
+package DKSPACE.PhamarERP.basecrud;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+
+public abstract class AbstractBaseCRUDController<E extends BaseCRUDEntity, S extends BaseCRUDService<E>> {
+    protected final S service;
+
+    protected AbstractBaseCRUDController(S service) {
+        this.service = service;
+    }
+    @PostMapping
+    public ResponseEntity<E> create(@RequestBody E entity) {
+        return ResponseEntity.ok(service.save(entity));
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<E> getDetail(@PathVariable Long id) {
+        return ResponseEntity.ok(service.findOne(id));
+    }
+
+    @PutMapping
+    public ResponseEntity<E> update(@RequestBody E entity) {
+        return ResponseEntity.ok(service.partialUpdate(entity));
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> delete(@PathVariable Long id) {
+        service.softDelete(id);
+        return ResponseEntity.noContent()
+                             .build();
+    }
+}
---
+++ src/main/java/DKSPACE/PhamarERP/basecrud/BaseCRUDRepository.java
@@ -0,0 +1,13 @@
+package DKSPACE.PhamarERP.basecrud;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
+import org.springframework.data.repository.NoRepositoryBean;
+
+import java.util.Optional;
+
+@NoRepositoryBean
+public interface BaseCRUDRepository<E extends BaseCRUDEntity, ID> extends JpaRepository<E, ID> ,
+        JpaSpecificationExecutor<E> {
+    Optional<E> findByIdAndDeletedFlagFalse(Long Id);
+}
