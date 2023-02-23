a80594c7045704c4c85970ce42aaa40d439ceed5
huy8895 <huytv.ous@mcredit.com.vn>
2023-02-23 15:17:36 +0700

    [baseCRUD] refactor abstract controller

--- src/main/java/DKSPACE/PhamarERP/basecrud/AbstractBaseCRUDController.java
+++ src/main/java/DKSPACE/PhamarERP/basecrud/AbstractBaseCRUDController.java
@@ -6,8 +6,7 @@ import jakarta.validation.Valid;
 import org.springdoc.core.annotations.ParameterObject;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -17,7 +16,7 @@ import java.util.List;
 public abstract class AbstractBaseCRUDController<E extends BaseCRUDEntity, S extends BaseCRUDService<E>> {
     protected final S service;
     protected final Class<E> entity;
-    private final ExcelHelper excelHelper;
+    protected final ExcelHelper excelHelper;
 
     protected AbstractBaseCRUDController(S service, Class<E> entity) {
         this.service = service;
@@ -59,7 +58,9 @@ public abstract class AbstractBaseCRUDController<E extends BaseCRUDEntity, S ext
 
     @GetMapping("/export-template")
     public ResponseEntity<byte[]> exportTemplate() {
-        return ResponseEntity.ok(excelHelper.exportTemplate(this.entity));
+        return ResponseEntity.status(HttpStatus.OK)
+                             .headers(this.getHttpHeaders("-template-" + this.entity.getSimpleName()))
+                             .body(excelHelper.exportTemplate(this.entity));
     }
 
     @PostMapping(value = "/import", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
@@ -69,7 +70,16 @@ public abstract class AbstractBaseCRUDController<E extends BaseCRUDEntity, S ext
 
     @GetMapping("/export")
     public ResponseEntity<byte[]> importFileExcel() {
-        return ResponseEntity.ok(excelHelper.writeFile(service.findAll(Pageable.unpaged())
-                                                              .getContent(), this.entity));
+        return ResponseEntity.status(HttpStatus.OK)
+                             .headers(this.getHttpHeaders("-export-" + entity.getSimpleName()))
+                             .body(excelHelper.writeFile(service.findAll(Pageable.unpaged())
+                                                                .getContent(), this.entity));
+    }
+
+    private HttpHeaders getHttpHeaders(String filename) {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.parseMediaType("application/vnd.openxmlformats-officedocument.spreadsheetml.sheet;charset=UTF-8"));
+        headers.set(HttpHeaders.CONTENT_DISPOSITION, "inline; filename=" + System.currentTimeMillis() + filename + ".xls");
+        return headers;
     }
 }
--- src/main/java/DKSPACE/PhamarERP/master_data/sample/SampleController.java
+++ src/main/java/DKSPACE/PhamarERP/master_data/sample/SampleController.java
@@ -2,6 +2,7 @@ package DKSPACE.PhamarERP.master_data.sample;
 
 import DKSPACE.PhamarERP.basecrud.AbstractBaseCRUDController;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
