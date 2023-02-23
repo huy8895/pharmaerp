ecd3365bc7e9b600cddce7657ffb689fb9903b18
huy8895 <huytv.ous@mcredit.com.vn>
2023-02-23 13:52:11 +0700

    [baseCRUD] refactor abstract controller

--- src/main/java/DKSPACE/PhamarERP/basecrud/AbstractBaseCRUDController.java
+++ src/main/java/DKSPACE/PhamarERP/basecrud/AbstractBaseCRUDController.java
@@ -1,25 +1,35 @@
 package DKSPACE.PhamarERP.basecrud;
 
+import DKSPACE.PhamarERP.helper.excel.ExcelHelper;
+import DKSPACE.PhamarERP.helper.excel.impl.ExcelHelperImpl;
 import jakarta.validation.Valid;
 import org.springdoc.core.annotations.ParameterObject;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
 
 public abstract class AbstractBaseCRUDController<E extends BaseCRUDEntity, S extends BaseCRUDService<E>> {
     protected final S service;
+    protected final Class<E> entity;
+    private final ExcelHelper excelHelper;
 
-    protected AbstractBaseCRUDController(S service) {
+    protected AbstractBaseCRUDController(S service, Class<E> entity) {
         this.service = service;
+        this.entity = entity;
+        this.excelHelper = new ExcelHelperImpl();
     }
+
     @PostMapping
     public ResponseEntity<E> create(@RequestBody @Valid E entity) {
         return ResponseEntity.ok(service.save(entity));
     }
+
     @PostMapping("/save-all")
     public ResponseEntity<List<E>> saveAll(@RequestBody @Valid List<E> entity) {
         return ResponseEntity.ok(service.saveAll(entity));
@@ -45,5 +55,21 @@ public abstract class AbstractBaseCRUDController<E extends BaseCRUDEntity, S ext
         service.softDelete(id);
         return ResponseEntity.noContent()
                              .build();
+    }
+
+    @GetMapping("/export-template")
+    public ResponseEntity<byte[]> exportTemplate() {
+        return ResponseEntity.ok(excelHelper.exportTemplate(this.entity));
+    }
+
+    @PostMapping(value = "/import", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<List<E>> exportFileExcel(@RequestParam("file") MultipartFile file) {
+        return ResponseEntity.ok(excelHelper.readFile(file, this.entity));
+    }
+
+    @GetMapping("/export")
+    public ResponseEntity<byte[]> importFileExcel() {
+        return ResponseEntity.ok(excelHelper.writeFile(service.findAll(Pageable.unpaged())
+                                                              .getContent(), this.entity));
     }
 }
--- src/main/java/DKSPACE/PhamarERP/master_data/sample/SampleController.java
+++ src/main/java/DKSPACE/PhamarERP/master_data/sample/SampleController.java
@@ -1,37 +1,15 @@
 package DKSPACE.PhamarERP.master_data.sample;
 
 import DKSPACE.PhamarERP.basecrud.AbstractBaseCRUDController;
-import DKSPACE.PhamarERP.helper.excel.ExcelHelper;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.data.domain.Pageable;
-import org.springframework.http.MediaType;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
-import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @Slf4j
 @RestController
 @RequestMapping("/api/sample")
 public class SampleController extends AbstractBaseCRUDController<SampleEntity, SampleService> {
-    private final ExcelHelper excelHelper;
-
-    protected SampleController(SampleService service, ExcelHelper excelHelper) {
-        super(service);
-        this.excelHelper = excelHelper;
-    }
-
-    @GetMapping("/export-template")
-    public ResponseEntity<?> exportTemplate() {
-        return ResponseEntity.ok(excelHelper.exportTemplate(SampleEntity.class));
-    }
-
-    @PostMapping(value = "/import", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
-    public ResponseEntity<?> exportFileExcel(@RequestParam("file") MultipartFile file) {
-        return ResponseEntity.ok(excelHelper.readFile(file, SampleEntity.class));
-    }
-    @GetMapping("/export")
-    public ResponseEntity<?> importFileExcel() {
-        return ResponseEntity.ok(excelHelper.writeFile(service.findAll(Pageable.unpaged())
-                                                              .getContent(), SampleEntity.class));
+    protected SampleController(SampleService service) {
+        super(service, SampleEntity.class);
     }
 }
