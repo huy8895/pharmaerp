bb989631cc41f6f96ecbe0d67867144230b92005
huy8895 <huytv.ous@mcredit.com.vn>
2023-02-24 09:44:50 +0700

    [database] update user.

--- src/main/java/DKSPACE/PhamarERP/auth/model/User.java
+++ src/main/java/DKSPACE/PhamarERP/auth/model/User.java
@@ -105,7 +105,8 @@ public class User implements UserDetails {
 
     @Override
     public Collection<? extends GrantedAuthority> getAuthorities() {
-        return List.of(new SimpleGrantedAuthority(role.name()));
+//        return List.of(new SimpleGrantedAuthority(role.name())); todo: set list roles
+        return List.of();
     }
 
     @Override
