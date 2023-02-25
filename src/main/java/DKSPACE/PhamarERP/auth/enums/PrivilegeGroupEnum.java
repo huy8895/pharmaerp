dadebf448724d9f52169b4b0f11f8f97cdacd351
huy8895 <huy8895@gmail.com>
2023-02-25 21:12:41 +0700

    [quanlynhansu] create base enums #2

--- src/main/java/DKSPACE/PhamarERP/auth/enums/PrivilegeGroupEnum.java
+++ src/main/java/DKSPACE/PhamarERP/auth/enums/PrivilegeGroupEnum.java
@@ -1,13 +1,27 @@
 package DKSPACE.PhamarERP.auth.enums;
 
+import org.apache.poi.ss.formula.functions.Days;
+
+import java.util.*;
+
 import static DKSPACE.PhamarERP.auth.enums.PrivilegeKeyEnum.*;
 
+interface hasPrivileges {
+    Map<PrivilegeKeyEnum, String> getPrivilegesMap();
+}
+
+
 public enum PrivilegeGroupEnum {
-    Role(CREATE,UPDATE),
+    Role("Phân quyền", MapUtils.<PrivilegeKeyEnum, String>build().put(
+            UPDATE,"chỉnh sửa"
+    ).ok()),
+
+    //todo: i18n
 
     ;
 
 
-    PrivilegeGroupEnum(PrivilegeKeyEnum... keys) {
+    PrivilegeGroupEnum(String groupName, Map<PrivilegeKeyEnum, String> map) {
+
     }
 }
