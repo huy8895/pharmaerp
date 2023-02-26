bea2e2cec15dec33cb8b2b300607074616770716
huy8895 <huy8895@gmail.com>
2023-02-26 22:03:41 +0700

    [quanlynhansu] create new entity

---
+++ src/main/java/DKSPACE/PhamarERP/master_data/entity/GenDepartment.java
@@ -0,0 +1,95 @@
+package DKSPACE.PhamarERP.master_data.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Size;
+
+import java.time.Instant;
+
+@Entity
+@Table(name = "gen_departments")
+public class GenDepartment {
+    @Id
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Size(max = 255)
+    @Column(name = "name")
+    private String name;
+
+    @Column(name = "describe", length = Integer.MAX_VALUE)
+    private String describe;
+
+    @NotNull
+    @Column(name = "is_active", nullable = false)
+    private Short isActive;
+
+    @Column(name = "created_at")
+    private Instant createdAt;
+
+    @Column(name = "updated_at")
+    private Instant updatedAt;
+
+    @Column(name = "deleted_at")
+    private Instant deletedAt;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescribe() {
+        return describe;
+    }
+
+    public void setDescribe(String describe) {
+        this.describe = describe;
+    }
+
+    public Short getIsActive() {
+        return isActive;
+    }
+
+    public void setIsActive(Short isActive) {
+        this.isActive = isActive;
+    }
+
+    public Instant getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Instant createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Instant getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Instant updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+
+    public Instant getDeletedAt() {
+        return deletedAt;
+    }
+
+    public void setDeletedAt(Instant deletedAt) {
+        this.deletedAt = deletedAt;
+    }
+
+}
