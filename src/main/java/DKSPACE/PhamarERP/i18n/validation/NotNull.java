954c8016dae0b0b9f4ea269731948c32f9b91403
huy8895 <huytv.ous@mcredit.com.vn>
2023-02-27 17:35:50 +0700

    [validate] update NotNull.java

--- src/main/java/DKSPACE/PhamarERP/i18n/validation/NotNull.java
+++ src/main/java/DKSPACE/PhamarERP/i18n/validation/NotNull.java
@@ -5,14 +5,19 @@ import DKSPACE.PhamarERP.i18n.validation.validator.NotNullValidator;
 import jakarta.validation.Constraint;
 import jakarta.validation.Payload;
 
-import java.lang.annotation.*;
+import java.lang.annotation.Documented;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+import static java.lang.annotation.ElementType.*;
 
 /**
  * Khai báo một custom annotation
  */
 @Documented
 @Constraint(validatedBy = NotNullValidator.class)
-@Target({ElementType.METHOD, ElementType.FIELD})
+@Target({ METHOD, FIELD, ANNOTATION_TYPE, CONSTRUCTOR, PARAMETER, TYPE_USE })
 @Retention(RetentionPolicy.RUNTIME)
 public @interface NotNull {
     String message() default ValidateCode.NOT_NULL;
