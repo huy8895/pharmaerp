4889d20f74754bbae12a65a7f136374728c5215f
huy8895 <huytv.ous@mcredit.com.vn>
2023-03-02 10:02:52 +0700

    [permission] update DefaultDataConfig.java

--- src/main/java/DKSPACE/PhamarERP/auth/config/DefaultDataConfig.java
+++ src/main/java/DKSPACE/PhamarERP/auth/config/DefaultDataConfig.java
@@ -90,7 +90,7 @@ public class DefaultDataConfig {
                    .build();
     }
 
-    private List<Permission> setupPermissions() {
+    private void setupPermissions() {
         log.info("setupPermissions");
         Set<Permission> collect = Arrays.stream(PermissionGroupEnum.values())
                                         .map(this::buildPermission)
@@ -98,12 +98,19 @@ public class DefaultDataConfig {
                                         .collect(Collectors.toSet());
 
         List<Permission> permissionAll = permissionRepository.findAll();
-        if (!permissionAll.isEmpty()) {
+        List<Permission> newPermission = collect.stream()
+                                                .filter(permission -> permissionAll.stream()
+                                                                                   .noneMatch(p -> p.getKey()
+                                                                                                    .equals(permission.getKey())))
+                                                .toList();
+
+        if (newPermission.isEmpty()) {
             log.info("Permission already import");
-            return permissionAll;
+            return;
         }
-        log.info("setupPermissions = " + collect);
-        return permissionRepository.saveAll(collect);
+        log.info("setupPermissions = {}", newPermission);
+        permissionRepository.saveAll(newPermission);
+        return;
     }
 
     private List<Permission> buildPermission(PermissionGroupEnum groupEnum) {
