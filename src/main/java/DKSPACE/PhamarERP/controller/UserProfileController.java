a3c1b4b2c5f99b3181842a806da78a0634856411
huy8895 <huytv.ous@mcredit.com.vn>
2023-03-13 16:43:06 +0700

    [userprofile] fix private to public

--- src/main/java/DKSPACE/PhamarERP/controller/UserProfileController.java
+++ src/main/java/DKSPACE/PhamarERP/controller/UserProfileController.java
@@ -4,6 +4,7 @@ import DKSPACE.PhamarERP.auth.aop.HasPermission;
 import DKSPACE.PhamarERP.auth.enums.permission.PermissionKeyEnum;
 import DKSPACE.PhamarERP.master_data.dto.user_profile.UserProfileReqDto;
 import DKSPACE.PhamarERP.service.UserProfileService;
+import io.swagger.v3.oas.annotations.security.SecurityRequirement;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/api/user-profiles")
 @RequiredArgsConstructor
+@SecurityRequirement(name = "bearerAuth")
 public class UserProfileController {
 	private final UserProfileService service;
 	
@@ -21,7 +23,7 @@ public class UserProfileController {
 	 */
 	@GetMapping("/{userId}")
 	@HasPermission(value = PermissionKeyEnum.VIEW_USER_PROFILE, userId = "#{#userId}")
-	private Object getUserProfile(@PathVariable("userId") Long userId){
+	public Object getUserProfile(@PathVariable("userId") Long userId){
 		return service.getUserProfile(userId);
 	}
 	
@@ -31,7 +33,7 @@ public class UserProfileController {
 	 */
 	@PutMapping
 	@HasPermission(value = PermissionKeyEnum.UPDATE_USER_PROFILE, userId = "#{#dto.id}")
-	private Object updateUserProfile(@Valid @RequestBody UserProfileReqDto dto){
+	public Object updateUserProfile(@Valid @RequestBody UserProfileReqDto dto){
 		return service.updateUserProfile(dto);
 	}
 }
