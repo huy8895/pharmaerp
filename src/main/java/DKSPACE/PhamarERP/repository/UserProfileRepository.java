cf57f32c4cbbd93b58e4b012fb9e6f9dbc609a12
huy8895 <huy8895@gmail.com>
2023-03-14 00:40:46 +0700

    [userprofile] create base api update and get userprofile

--- src/main/java/DKSPACE/PhamarERP/repository/UserProfileRepository.java
+++ src/main/java/DKSPACE/PhamarERP/repository/UserProfileRepository.java
@@ -2,6 +2,12 @@ package DKSPACE.PhamarERP.repository;
 
 import DKSPACE.PhamarERP.basecrud.BaseCRUDRepository;
 import DKSPACE.PhamarERP.master_data.entity.UserProfile;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+
+import java.util.Optional;
 
 public interface UserProfileRepository extends BaseCRUDRepository<UserProfile, Long> {
+	@Query("select u from UserProfile u where u.user.id = :userId")
+	Optional<UserProfile> findByUserId(@Param("userId") Long userId);
 }
--- src/main/java/DKSPACE/PhamarERP/service/impl/UserProfileServiceImpl.java
+++ src/main/java/DKSPACE/PhamarERP/service/impl/UserProfileServiceImpl.java
@@ -1,10 +1,12 @@
 package DKSPACE.PhamarERP.service.impl;
 
+import DKSPACE.PhamarERP.auth.model.User;
 import DKSPACE.PhamarERP.basecrud.AbstractBaseCRUDService;
 import DKSPACE.PhamarERP.master_data.dto.user_profile.UserProfileReqDto;
 import DKSPACE.PhamarERP.master_data.entity.UserProfile;
 import DKSPACE.PhamarERP.repository.UserProfileRepository;
 import DKSPACE.PhamarERP.service.UserProfileService;
+import DKSPACE.PhamarERP.service.UserService;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
@@ -12,17 +14,31 @@ import org.springframework.stereotype.Service;
 @Slf4j
 @Service
 public class UserProfileServiceImpl extends AbstractBaseCRUDService<UserProfile, UserProfileRepository> implements UserProfileService {
-	protected UserProfileServiceImpl(UserProfileRepository repository) {
+	private final UserService userService;
+	protected UserProfileServiceImpl(UserProfileRepository repository,
+	                                 UserService userService) {
 		super(repository);
+		this.userService = userService;
+	}
+	
+	@Override
+	public UserProfile findOne(Long userID) {
+		return repository.findByUserId(userID)
+		                 .orElse(new UserProfile());
 	}
 	
 	@Override
 	public Object getUserProfile(Long userId) {
-		return userId;
+		return this.findOne(userId);
 	}
 	
 	@Override
 	public Object updateUserProfile(UserProfileReqDto dto) {
-		return null;
+		User user = userService.findOne(dto.getId());
+		UserProfile userProfile = this.save(UserProfile.builder()
+		                                        .user(user)
+		                                        .build());
+		return this.partialUpdate()
+				;
 	}
 }
