e0e8cf44f04681d1b21b274ae9a4847ddfc8d0b7
huy8895 <huy8895@gmail.com>
2023-03-19 08:57:57 +0700

    [baseentity] update AbstractBaseCRUDController.java

--- src/main/java/DKSPACE/PhamarERP/basecrud/AbstractBaseCRUDController.java
+++ src/main/java/DKSPACE/PhamarERP/basecrud/AbstractBaseCRUDController.java
@@ -2,6 +2,7 @@ package DKSPACE.PhamarERP.basecrud;
 
 import DKSPACE.PhamarERP.helper.excel.ExcelHelper;
 import DKSPACE.PhamarERP.helper.excel.impl.ExcelHelperImpl;
+import io.swagger.v3.oas.annotations.Operation;
 import jakarta.validation.Valid;
 import org.springdoc.core.annotations.ParameterObject;
 import org.springframework.data.domain.Page;
@@ -12,7 +13,12 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 
-
+/**
+ * Lớp trừu tượng cho các controller CRUD cơ bản
+ * - Các controller này sẽ có các phương thức chung như: tạo, lưu danh sách, lấy chi tiết, lấy tất cả, cập nhật và xóa
+ * - Các controller này sẽ sử dụng một service cơ bản và một entity cơ bản
+ * - Các controller này cũng có thể xuất và nhập file Excel cho entity của chúng
+ **/
 public abstract class AbstractBaseCRUDController<E extends BaseCRUDEntity, S extends BaseCRUDService<E>> {
     protected final S service;
     protected final Class<E> entity;
@@ -25,31 +31,37 @@ public abstract class AbstractBaseCRUDController<E extends BaseCRUDEntity, S ext
     }
 
     @PostMapping
+    @Operation(summary="Tạo mới")
     public Object create(@RequestBody @Valid E entity) {
         return service.save(entity);
     }
 
     @PostMapping("/save-list")
+    @Operation(summary="Lưu danh sách")
     public Object saveList(@RequestBody @Valid List<E> entity) {
         return service.saveList(entity);
     }
 
     @GetMapping("/{id}")
+    @Operation(summary="Lấy thông tin chi tiết")
     public Object getDetail(@PathVariable Long id) {
         return service.findOne(id);
     }
 
     @GetMapping
+    @Operation(summary="Lấy danh sách và lọc")
     public Object getAll(@ParameterObject Pageable pageable) {
         return service.findAll(pageable);
     }
 
     @PutMapping
+    @Operation(summary="Cập nhật")
     public Object update(@RequestBody E entity) {
         return service.partialUpdate(entity);
     }
 
     @DeleteMapping("/{id}")
+    @Operation(summary="Xóa")
     public ResponseEntity<Void> delete(@PathVariable Long id) {
         service.softDelete(id);
         return ResponseEntity.noContent()
@@ -57,6 +69,7 @@ public abstract class AbstractBaseCRUDController<E extends BaseCRUDEntity, S ext
     }
 
     @GetMapping("/export-template-import")
+    @Operation(summary="Xuất template để nhập")
     public ResponseEntity<byte[]> exportTemplate() {
         return ResponseEntity.status(HttpStatus.OK)
                              .headers(this.getHttpHeaders("-template-" + this.entity.getSimpleName()))
@@ -64,11 +77,13 @@ public abstract class AbstractBaseCRUDController<E extends BaseCRUDEntity, S ext
     }
 
     @PostMapping(value = "/import", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    @Operation(summary="Nhập dữ liệu từ file Excel")
     public Object exportFileExcel(@RequestParam("file") MultipartFile file) {
         return excelHelper.readFile(file, this.entity);
     }
 
     @GetMapping("/export")
+    @Operation(summary="Xuất dữ liệu ra file Excel")
     public ResponseEntity<byte[]> importFileExcel() {
         return ResponseEntity.status(HttpStatus.OK)
                              .headers(this.getHttpHeaders("-export-" + entity.getSimpleName()))
