f808bf1b010c0f05eb6faccc7fac602a07a317ce
huy8895 <huy8895@gmail.com>
2023-03-27 20:40:10 +0700

    [document] create repository

---
+++ src/main/java/DKSPACE/PhamarERP/document/repository/DocumentRepository.java
@@ -0,0 +1,7 @@
+package DKSPACE.PhamarERP.document.repository;
+
+import DKSPACE.PhamarERP.basecrud.BaseCRUDRepository;
+import DKSPACE.PhamarERP.document.model.Document;
+
+public interface DocumentRepository extends BaseCRUDRepository<Document, Long> {
+}
