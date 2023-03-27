74b537e833676737dd6c46bdaae9b948e7510b4b
huy8895 <huy8895@gmail.com>
2023-03-27 21:44:25 +0700

    [material] create material base api

--- src/main/java/DKSPACE/PhamarERP/material/criteria/MaterialCriteria.java
+++ src/main/java/DKSPACE/PhamarERP/material/criteria/MaterialCriteria.java
@@ -2,6 +2,10 @@ package DKSPACE.PhamarERP.material.criteria;
 
 import DKSPACE.PhamarERP.basecrud.query.BaseCrudCriteria;
 import DKSPACE.PhamarERP.material.model.Material;
+import DKSPACE.PhamarERP.material.model.Material_;
+import io.github.jhipster.service.filter.BooleanFilter;
+import io.github.jhipster.service.filter.LongFilter;
+import io.github.jhipster.service.filter.StringFilter;
 import jakarta.persistence.metamodel.SingularAttribute;
 import lombok.*;
 
@@ -13,8 +17,20 @@ import java.util.List;
 @NoArgsConstructor
 @AllArgsConstructor
 public class MaterialCriteria extends BaseCrudCriteria<Material> {
+	
+	private LongFilter genUnitId;
+	private LongFilter materialGroupId;
+	private StringFilter nameVi;
+	private StringFilter nameEn;
+	private StringFilter nameScience;
+	private StringFilter nameOther;
+	private LongFilter inventoryMin;
+	private LongFilter inventoryMax;
+	private StringFilter describe;
+	private BooleanFilter isActive;
+	
 	@Override
 	public List<SingularAttribute<Material, String>> searchBy() {
-		return List.of();
+		return List.of(Material_.nameVi, Material_.nameEn, Material_.nameScience, Material_.nameOther, Material_.describe);
 	}
 }
